0f14f606b41dfc04662b95e6241e40a59fd9225e
Manasi Chaudhary <mchaudh@ad.unc.edu>
2022-11-10 20:45:39 -0500

    changes

---
+++ content.py
@@ -0,0 +1,15 @@
+"""
+# Mission
+- Access to resources for women in CS/tech fields. 
+- Creating a fun, engaging work environment for club members. 
+- Provide oppurtunities within the field such as networking with partnering organizations or companies. 
+- Allow members to navigate the tech world through workshops, panels, and seminars. 
+
+
+# Officers
+
+
+
+
+# Members
+"""
